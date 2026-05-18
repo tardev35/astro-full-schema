4130f3b73331f4f9c6bc7aa5b7d50b0b1431deef
tardev35 <suksanban1992@gmail.com>
2026-05-18 20:49:29 +0700

    kong24h

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,6 +19,6 @@ export default defineConfig({
     '/line': 'https://tinyurl.com/linepigpg'
   },
 
-  site: 'https://hydra888auto.com',
+  site: 'https://kong24h-main.com',
   integrations: [sitemap()]
 });

cbe1eccdc038930b482df434415025f069fb94ad
tardev35 <suksanban1992@gmail.com>
2026-05-20 20:19:47 +0700

    waiver388

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,6 +19,6 @@ export default defineConfig({
     '/line': 'https://tinyurl.com/linepigpg'
   },
 
-  site: 'https://vegus24r.com',
+  site: 'https://waiver388-homepage.com',
   integrations: [sitemap()]
 });
